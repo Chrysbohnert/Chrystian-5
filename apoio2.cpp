3d25a5186e79fd50ac85036d77550d11d6b94bbb
ChrysBohnert <cb0127433@setrem.com.br>
2022-09-29 22:11:49 -0300

    primeiro commit

---
+++ apoio2.cpp
@@ -0,0 +1,19 @@
+#include <map>
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    string palavra;
+    cout << "Digite uma palavra: \n";
+    cin >> palavra;
+    map<char,int> contarletras;
+    for (unsigned int i=0; i<palavra.size(); i++)
+        contarletras[palavra[i]]++;
+
+    for (map<char, int>::iterator it = contarletras.begin(); it != contarletras.end(); ++it)
+        cout << it->first << ": " << it->second << endl;
+
+    return 0;
+}
